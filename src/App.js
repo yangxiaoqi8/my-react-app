d634ad69a24008fc7e331207bee7c79c5011c5eb
yangxiaoqi8 <979256993@qq.com>
2020-10-19 22:21:33 +0800

    [Feature]简单组件制作和绑定事件添加

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button/Button.js';
-
+//事件绑定 onClick={函数名}
+// 例如：<div onClick={tick}>test</div>
 
 function App() {
+  // 事件方法
+  function tick() {
+    console.log("yxqTest");
+    console.log(new Date().toLocaleTimeString());
+  }
+  setInterval(tick,1000)
+
+  //组件制作
+  function ElementDiv() {
+    return (
+        <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>      
+    )     
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +39,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <div onClick={tick}>test</div>
+      <ElementDiv></ElementDiv>    
       <Button/>
     </div>
   );
